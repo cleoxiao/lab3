1e127619511231321074e0ed25a1c1769b509989
Yanxin Xiao <yax064@ucsd.edu>
2020-01-23 03:49:49 -0800

    node js

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,12 @@
  */
 
 exports.view = function(req, res){
+  var nameTOSHOW = req.params.UserName;
+  if(nameTOSHOW === undefined){
+    nameTOSHOW = 'Yanxin';
+  }
+  console.log("Name is "+nameTOSHOW);
   res.render('index', {
-  	'name': '',
+  	'name': nameTOSHOW,
   });
 };
